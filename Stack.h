e59a8eddd2df01645401f49f1459128f01a69a52
KHoutas <39933124+KHoutas@users.noreply.github.com>
2018-06-04 21:36:10 -0400

    Edited the look of the comments. (indentation)

--- Stack.h
+++ Stack.h
@@ -2,7 +2,7 @@
 
 // Stack class that handles all of the stack information through nodes (linked implementation)
 
-template <class ItemType>		// Using template classes to allow for diverse data types
+template <class ItemType>			// Using template classes to allow for diverse data types
 class Stack
 {
 private:
@@ -13,23 +13,23 @@ private:
 	private:
 
 		ItemType data;			// Holds the data of whatever data type we input
-		Node* next;				// Holds the location of the next node in this stack, which we are pointing to.
+		Node* next;			// Holds the location of the next node in this stack, which we are pointing to.
 
 	public:
 
-		Node(ItemType, Node*);	//Constructor for the class, allows to initialize data and next node.
+		Node(ItemType, Node*);		//Constructor for the class, allows to initialize data and next node.
 		ItemType getData();		// Gets the data information
 		Node* getNext();		// Gets the location of the next node
 
 	};
 
-	Node* base;					// The location to our base node, the top of the stack
-	int size;					// The amount of items we currently have in the stack.
+	Node* base;				// The location to our base node, the top of the stack
+	int size;				// The amount of items we currently have in the stack.
 
 public:
 
-	Stack();					// Default constructor for the stack
-	void push(ItemType);		// Function that pushes items onto the stack
+	Stack();				// Default constructor for the stack
+	void push(ItemType);			// Function that pushes items onto the stack
 	ItemType pop();				// Function that pops items off of the stack
 	ItemType peek();			// Function that takes a look at the item on the top of the stack
 	bool isEmpty();				// Function that checks if the stack is empty
